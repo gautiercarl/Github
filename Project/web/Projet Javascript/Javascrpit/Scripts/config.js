231a2b767e594cdb3af42fd13af42f4d1cac07c0
gautiercarl <gautiercarl8@gmail.com>
2024-12-15 15:36:26 +0100

    delete

--- Project/web/Projet Javascript/Javascrpit/Scripts/config.js
+++
@@ -1,7 +0,0 @@
-let listeMots = ["Cachalot", "Pétunia", "Serviette"];
-let listePhrase = [
-  "Pas de panique !",
-  "La vie, l’univers et le reste",
-  "Merci pour le poisson",
-];
-let listeFélicitations = ["Félicitations!", "waouh!", "genie"];
--- Project/web/Projet Javascript/Javascrpit/tuto.js
+++
@@ -1,8 +0,0 @@
-const MonPrenom= "Carlsons"
-let MonAge= 21
-console.log("j'ai",MonAge,"ans")
-
-MonAge+=1
-console.log("l'an prochain j'ai",MonAge,"ans")
-
-console.log("et toi quel est ton age?")
